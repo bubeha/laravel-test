c2b8a8111957052139c9208b0081642362bd1e03
Alexandr Bubeshko <a.bubeshko@gmail.com>
2024-01-12 23:39:08 +0300

    fix: resolve problem with cache saving

--- app/Http/Middleware/CacheMiddleware.php
+++ app/Http/Middleware/CacheMiddleware.php
@@ -5,19 +5,17 @@ declare(strict_types=1);
 namespace App\Http\Middleware;
 
 use App\Helpers\CacheHelper;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Cache;
 
 final readonly class CacheMiddleware
 {
-    public function handle($request, \Closure $next)
+    public function handle(Request $request, \Closure $next)
     {
-        if ('production' !== config('app.env')) {
-            return $next($request);
-        }
-
+        // todo resolve problem with caching empty response
         return Cache::tags(['item'])
             ->remember(
-                CacheHelper::generateRouteKey($request->url()),
+                CacheHelper::generateRouteKey($request->fullUrl()),
                 config('cache.route.ttl'),
                 static fn () => $next($request));
     }
