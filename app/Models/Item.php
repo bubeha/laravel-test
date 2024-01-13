8fa63e0cc8e50db3212adbbc85e020631a035dd7
Alexandr Bubeshko <a.bubeshko@gmail.com>
2024-01-13 16:12:43 +0300

    docs: add phpdoc to Item model

--- app/Models/Item.php
+++ app/Models/Item.php
@@ -7,6 +7,21 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * App\Models\Item
+ *
+ * @property int $id
+ * @property string $name
+ * @property string $url
+ * @method static \Database\Factories\ItemFactory factory($count = null, $state = [])
+ * @method static \Illuminate\Database\Eloquent\Builder|Item newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|Item newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|Item query()
+ * @method static \Illuminate\Database\Eloquent\Builder|Item whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Item whereName($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Item whereUrl($value)
+ * @mixin \Eloquent
+ */
 final class Item extends Model
 {
     use HasFactory;
